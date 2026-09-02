cbdd6fcdd7ac9a5b91cc2186f715120511a929b0
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-02 10:53:02 -0300

    Refactor ContaBancaria class with methods for banking

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
@@ -44,4 +44,42 @@ public class ContaBancaria {
     }
 }
 
+---
+
+    public class ContaBancaria {
+    private int numConta, agencia;
+    public String titular, tipoConta;
+    protected double saldo = 0.00;
+    public final double TAXA_MANUTENCAO_CONTA = 5.00;
+
+    public ContaBancaria(int numConta, int agencia, String titular, String tipoConta, double saldo){
+
+        this.numConta = numConta;
+        this.agencia = agencia;
+        this.titular = titular;
+        this.tipoConta = tipoConta;
+        this.saldo = 0.00;
+    }
+
+    public int getNumConta(){
+        return numConta;
+    }
+
+    public void setNunConta(int numConta){
+        this.numConta = numConta;
+    }
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+
+    public double consultarSaldo(){
+        saldo = saldo - TAXA_MANUTENCAO_CONTA;
+        return saldo;
+    }
+}
 
