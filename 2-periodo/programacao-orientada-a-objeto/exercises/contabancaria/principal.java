5c14233810793adc8091840023dd46e46ad317dd
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-02 09:47:33 -0300

    Update principal.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/principal.java
@@ -1,7 +1,6 @@
 public class Principal {
     public static void main (String[] args){
         ContaBancaria contaPF = new ContaBancaria();
-        ContaBancaria contapF = new ContaBancaria();
         ContaBancaria contaPj = new ContaBancaria();
 
         contaPF.numConta = 265486;
@@ -19,6 +18,43 @@ public class Principal {
 
         contaPj.depositar(500.00);
         contaPj.sacar(50.00);
+    }
+}
+
+---
+
+    public class Principal {
+    public static void main (String[] args){
+        ContaBancaria contaPF = new ContaBancaria(00000, 658, null, null);
+        ContaBancaria contaPj = new ContaBancaria(00000, 658, null, null);
+
+        //Criando informações de um objeto pessoal física
+        contaPF.numConta = 265486;
+        contaPF.titular = "Eduardo da Silva";
+        contaPF.tipoConta = "Pessoa Fisica";
+
+        contaPF.depositar(500.00);
+        contaPF.sacar(50.00);
+        
+       // Criando informações de  um objeto pessoa juridica
+        contaPj.numConta = 632659;
+        contaPj.titular = "Padaria O Sonho";
+        contaPj.tipoConta = "Pessoa Juridica";
+
+        contaPj.depositar(500.00);
+        contaPj.sacar(50.00);
+        
+        // Converter um valor double para um valor inteiro
+        double total = 250.35;
+        int totalConvertido = (int) total;
+        
+        //Converter um valor de string para um valor inteiro
+        String idade = "25";
+        int idadeConvertida = Integer.parseInt(idade);
+        
+        //Converter um valor boolean para String
+        boolean aceita = true;
+        String aceitaConvertido = String.valueOf(aceita);
     }
 }
 
