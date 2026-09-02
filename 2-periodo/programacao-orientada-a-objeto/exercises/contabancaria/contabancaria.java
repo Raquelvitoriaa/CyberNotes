ae515f9b73c9b9f64cc600541c1f43288ed8865e
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-02 09:48:22 -0300

    Add ContaBancaria class with deposit and withdraw methods

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
@@ -13,3 +13,30 @@ public class ContaBancaria {
     }
 }
 
+---
+
+    public class ContaBancaria {
+    int numConta, agencia;
+    String titular, tipoConta;
+    double saldo = 0.00;
+    final double TAXA_MANUTENCAO_CONTA = 5.00;
+
+    public ContaBancaria(int numConta, int agencia, String titular, String tipoConta, double saldo){
+
+        this.numConta = numConta;
+        this.agencia = agencia;
+        this.titular = titular;
+        this.tipoConta = tipoConta;
+        this.saldo = 0.00;
+    }
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+}
+
+
