952299c27ce47c8ddb1ec408be940c8731d8810a
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-02 09:11:57 -0300

    Update contabancaria.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
@@ -1,1 +1,15 @@
+public class ContaBancaria {
+    int numConta, agencia;
+    String titular, tipoConta;
+    double saldo = 0.00;
+    final double TAXA_MANUTENCAO_CONTA = 5.00;
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+}
 
