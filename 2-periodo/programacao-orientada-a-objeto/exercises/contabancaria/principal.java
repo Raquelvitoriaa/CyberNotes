7ff13a0321e5a541589e0d0faf95d593d6dfe0d9
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-02 09:13:21 -0300

    Implement main method for ContaBancaria examples

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/principal.java
@@ -1,1 +1,24 @@
+public class Principal {
+    public static void main (String[] args){
+        ContaBancaria contaPF = new ContaBancaria();
+        ContaBancaria contapF = new ContaBancaria();
+        ContaBancaria contaPj = new ContaBancaria();
+
+        contaPF.numConta = 265486;
+        contaPF.agencia = 105;
+        contaPF.titular = "Eduardo da Silva";
+        contaPF.tipoConta = "Pessoa Fisica";
+
+        contaPF.depositar(500.00);
+        contaPF.sacar(50.00);
+
+        contaPj.numConta = 632659;
+        contaPj.agencia = 658;
+        contaPj.titular = "Padaria O Sonho";
+        contaPj.tipoConta = "Pessoa Juridica";
+
+        contaPj.depositar(500.00);
+        contaPj.sacar(50.00);
+    }
+}
 
