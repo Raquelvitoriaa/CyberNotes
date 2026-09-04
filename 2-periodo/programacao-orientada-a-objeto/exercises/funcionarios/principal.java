ee119d0a5e9c4ba57553974d4126dfba00cb6e9a
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-04 00:44:56 -0300

    Create main method to instantiate Funcionario objects
    Initialized two Funcionario objects but left fields unassigned.

--- 2-periodo/programacao-orientada-a-objeto/exercises/funcionarios/principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/funcionarios/principal.java
@@ -1,5 +1,13 @@
 public class Principal {
-    public static void main (String[] args){
-        Funcionario contaPF = new Funcionarios;
+    public static void main (String[] args) {
+        
+        Funcionario func1 = new Funcionario();
+        Funcionario func2 = new Funcionario();
+
+        func1.Nome = ;
+        func1.cpf = ;
+        func1.cargo = ;
+        func1.salarioMensal = ;
+        func1.departamento = ;
     }
 }
