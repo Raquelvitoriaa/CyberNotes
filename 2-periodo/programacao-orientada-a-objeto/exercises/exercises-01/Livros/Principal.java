ce497c0d416b72465d180d4bd6b47e6b70d3d6b8
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-09 11:02:41 -0300

    Update Principal.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/exercises-01/Livros/Principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/exercises-01/Livros/Principal.java
@@ -1,1 +1,33 @@
+public class LojaLivros {
+    public static void main(String[] args) {
+       Livro livroProgramacao = new Livro(null, null, null, null, 0, 0, 0.00);
+        Scanner input = new Scanner(System.in);
 
+
+       System.out.print("Digite o titulo do livro: ");
+       // String titulo = input.nextLine();
+       livroProgramacao.setTitulo(input.nextLine());
+       System.out.println("O título do livro é: \"" + livroProgramacao.getTitulo() + "\"");
+
+       System.out.print("Digite o nome do Autor: ");
+       livroProgramacao.setAutor(input.nextLine());
+       System.out.println("O nome do Autor é:" + livroProgramacao.getAutor());
+
+       System.out.print("Digite o numero de paginas: ");
+       livroProgramacao.setNumeroPaginas(input.nextInt());
+       System.out.println("O numero de paginas sao:" + livroProgramacao.getNumeroPaginas());
+
+       System.out.print("Digite o ano do livro: ");
+       livroProgramacao.setAnoPublicacao(input.nextInt());
+       System.out.println("O ano do livro é:" + livroProgramacao.getAnoPublicacao());
+
+       System.out.print("Digite o valor do livro: ");
+       livroProgramacao.setValor(input.nextInt());
+       System.out.println("O valor é: " + livroProgramacao.getValor());
+
+        input.close();
+    }
+}
+
+// System.out.print("O Autor é: " + livroProgramacao.getAutor() + "\n");
+      // System.out.printf("O ano do livro é: %d e o valor: %.2f", livroProgramacao.getAnoPublicacao(), livroProgramacao.getValor());
