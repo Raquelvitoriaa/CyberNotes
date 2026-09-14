817c128ceed870833be7be068c17a9651f703eab
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-14 08:54:57 -0300

    Create operadores-pre-incremento.c

---
+++ 2-periodo/estrutura-de-dados/codigos-testes-sala-de-aula/aula-03/operadores-pre-incremento.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //biblioteca padrão para comandos i/o
+
+int main (void)
+{
+int x=10, y=5, z;
+printf("x = %d\n\n", x);
+z = ++x + y;
+printf("y = %d\n\n", y);
+printf("z = ++x + y = %d\n\n", z);
+printf("x = %d\n\n", x);
+getchar();
+return 0;
+}
