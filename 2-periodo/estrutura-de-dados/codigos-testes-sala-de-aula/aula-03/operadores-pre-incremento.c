833ebe27bd1d6c533601251cf49bc0c983730f1e
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-14 08:59:55 -0300

    Fix pre-increment operation in operadores-pre-incremento.c
    Updated the pre-increment operation to clarify the result.

--- 2-periodo/estrutura-de-dados/codigos-testes-sala-de-aula/aula-03/operadores-pre-incremento.c
+++ 2-periodo/estrutura-de-dados/codigos-testes-sala-de-aula/aula-03/operadores-pre-incremento.c
@@ -4,7 +4,8 @@ int main (void)
 {
 int x=10, y=5, z;
 printf("x = %d\n\n", x);
-z = ++x + y;
+z = ++x + y; //16
+/* z = x++ + y; //15 */
 printf("y = %d\n\n", y);
 printf("z = ++x + y = %d\n\n", z);
 printf("x = %d\n\n", x);
