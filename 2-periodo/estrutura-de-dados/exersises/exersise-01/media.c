745d519f4057dc2794f38280eed5866834ab1609
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-14 09:31:11 -0300

    Create media.c

---
+++ 2-periodo/estrutura-de-dados/exersises/exersise-01/media.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+ {
+	float nota1, nota2, media;
+	printf("Digite sua primeira nota: \n");
+	scanf("%f", &nota1);
+	printf("Digite sua segunda nota: \n");
+	scanf("%f", &nota2);
+	
+	media = (nota1 + nota2) / 2;
+	
+	if(media <5)
+		printf("REPROVADO");
+	else if(media <7)
+		printf("RECUPERACAO");
+	else
+		printf("APROVADO!");
+	return 0;
+}
