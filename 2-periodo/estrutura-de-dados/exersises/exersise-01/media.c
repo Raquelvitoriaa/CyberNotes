34eb0adaf0303aaab7b3c45ec30181fd89764f40
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-14 09:40:38 -0300

    Update media.c

--- 2-periodo/estrutura-de-dados/exersises/exersise-01/media.c
+++ 2-periodo/estrutura-de-dados/exersises/exersise-01/media.c
@@ -16,7 +16,9 @@ int main()
 		printf("RECUPERACAO");
 	else
 		printf("APROVADO!");
+	 
 	 /* Operador Ternário
 	 printf(media < 5 ? "REPROVADO" : media <7 ? "RECUPERACAO" : "APROVADO");*/
+	 
 	return 0;
 }
