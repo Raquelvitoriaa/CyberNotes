90791587fb134c259d598067e99c4253e6b69db5
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-14 10:48:23 -0300

    Create aposentadoria.c

---
+++ 2-periodo/estrutura-de-dados/exersises/exersise-01/aposentadoria.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+ {
+	char sexo;
+	int tempo;
+	
+	printf("Digite seu sexo (M/F): ");
+	scanf("%c", &sexo);
+	printf("Digite sua idade: ");
+	scanf("%i", &tempo);
+	
+	sexo = toupper(sexo);
+	if(sexo=='M' && tempo >= 35 || sexo=='F' && tempo >= 25)
+		printf("APOSENTADO(A)");
+	else
+	{
+		printf("NA ATIVA!");
+	}
+	return 0;
+}
