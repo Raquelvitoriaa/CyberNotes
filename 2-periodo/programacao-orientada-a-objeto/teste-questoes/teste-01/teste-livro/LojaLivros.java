5122bde5df23df1d5e96794b4c966847bb769c2c
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-16 10:43:15 -0300

    Enhance LojaLivros with closing messages and input
    Added closing messages and input handling for book details.

--- 2-periodo/programacao-orientada-a-objeto/teste-questoes/teste-01/teste-livro/LojaLivros.java
+++ 2-periodo/programacao-orientada-a-objeto/teste-questoes/teste-01/teste-livro/LojaLivros.java
@@ -49,6 +49,8 @@ public class LojaLivros {
           default:
              System.out.println("Opção inválida. Digite outra opção!");
        }
+        System.out.println("FIM DO PROGRAMA!");
+       input.close();
 
 
        System.out.print("Digite o titulo do livro: ");
@@ -84,3 +86,50 @@ public class LojaLivros {
     }
 }
 
+
+
+/* System.out.print("Digite o titulo do livro: ");
+       // String titulo = input.nextLine();
+       livroProgramacao.setTitulo(input.nextLine());
+       //System.out.println("O título do livro é: \"" + livroProgramacao.getTitulo() + "\"");
+
+       System.out.print("Digite o nome do Autor: ");
+       livroProgramacao.setAutor(input.nextLine());
+       //System.out.println("O nome do Autor é:" + livroProgramacao.getAutor());
+
+       System.out.print("Digite o nome da Editora: ");
+       livroProgramacao.setEditora(input.nextLine());
+       //System.out.println("A Editora é: " + livroProgramacao.getEditora());
+
+       System.out.print("Digite a Isbn: " );
+       livroProgramacao.setIsbn(input.nextLine());
+       //System.out.println("A Isbn é: " + livroProgramacao.getIsbn());
+
+       System.out.print("Digite o numero de paginas: ");
+       livroProgramacao.setNumeroPaginas(input.nextInt());
+       //System.out.println("O numero de paginas sao:" + livroProgramacao.getNumeroPaginas());
+
+       System.out.print("Digite o ano do livro: ");
+       livroProgramacao.setAnoPublicacao(input.nextInt());
+       //System.out.println("O ano do livro é:" + livroProgramacao.getAnoPublicacao());
+
+       System.out.print("Digite o valor do livro: ");
+       livroProgramacao.setValor(input.nextInt());
+       //System.out.println("O valor é: " + livroProgramacao.getValor());
+
+       // System.out.print("O Autor é: " + livroProgramacao.getAutor() + "\n");
+      // System.out.printf("O ano do livro é: %d e o valor: %.2f", livroProgramacao.getAnoPublicacao(), livroProgramacao.getValor());
+
+       //System.out.print("Digite a quantidade de livros em estoque: ");
+       livroProgramacao.setEstoque(100);
+       System.out.println("\nQuantidade de livros em estoque " + livroProgramacao.consultarEstoque());
+
+       System.out.print("Digite o total de vendas diarias: ");
+       livroProgramacao.setVendasDiarias(input.nextInt());
+
+       System.out.println("\nO seu desempenho foi: " + livroProgramacao.calcularBonusCorporativo() + "\nE o seu bonus e: " + livroProgramacao.getBonusAtivo());
+
+       input.close();
+    }
+}
+*/
