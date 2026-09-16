a58d04b2ffb4b56920e5c69c0e808020de0b4692
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-16 10:33:57 -0300

    Update Livro.java

--- 2-periodo/programacao-orientada-a-objeto/teste-questoes/teste-01/teste-livro/Livro.java
+++ 2-periodo/programacao-orientada-a-objeto/teste-questoes/teste-01/teste-livro/Livro.java
@@ -1,1 +1,116 @@
+package acervo;
 
+public class Livro {
+    //Definição de Atributos
+    private String titulo;
+    private String autor;
+    private String editora;
+    private String isbn;
+    private int numeroPaginas;
+    private int anoPublicacao;
+    private double valor;
+    private int estoque;
+    private int vendasDiarias;
+    private double bonusAtivo;
+    private final int META = 150;
+
+    //Definição do Construtor
+    public Livro(String titulo, String autor, String editora, String isbn, int numeroPaginas, int anoPublicacao, double valor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.isbn = isbn;
+        this.numeroPaginas = numeroPaginas;
+        this.anoPublicacao = anoPublicacao;
+        this.valor = valor;
+    }
+
+    //Definição de Métodos de acesso Get e Set
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public void setVendasDiarias(int vendasDiarias){
+        this.vendasDiarias = vendasDiarias;
+    }
+
+    public double getBonusAtivo(){
+        return bonusAtivo;
+    }
+
+    public String consultarEstoque(){
+        return (estoque > 0) ? String.valueOf(estoque) : "Livro sem estoque";
+    }
+
+    public String calcularBonusCorporativo(){
+        String desempenhoFuncionario;
+        if (vendasDiarias >= META) {
+            desempenhoFuncionario = "Satisfatório";
+            bonusAtivo = 100;
+        } else if(vendasDiarias >= 50 && vendasDiarias < META) {
+            desempenhoFuncionario = "Pode melhorar";
+            bonusAtivo = 50;
+        } else {
+            desempenhoFuncionario = "Insatisfatório";
+            bonusAtivo = 0;
+        }
+        return desempenhoFuncionario;
+    }
+
+}
